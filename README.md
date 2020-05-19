bf4d11d90b87011b0081ceb8f0b0b87dac0a1a4c
Neokeld <arnaud.duforat@protonmail.ch>
2020-05-19 18:58:02 +0200

    Rapid javafx app design with custom css

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# JavaFXDesign
-Maven + JavaFX + OpenJDK 12
+# Maven + JavaFX + Openjdk 12 Delivery Management Sample UI JavaFX
+
+20200519 - Arnaud Duforat
+
+This project is : 
+* Maven + JavaFX + Openjdk 12
+* custom css
+* an example
+* a fiction with a delivery management company Deliverat
+* featuring Maïté
+* the violet color from https://v2.grommet.io/
+* no close button...
+* based on https://github.com/k33ptoo/RestaurantMgtSampleUI
---
+++ src/main/java/design/Main.java
@@ -0,0 +1,44 @@
+package design;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    private double x, y;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = loadFXML("Home");
+        primaryStage.setScene(new Scene(root));
+        //set stage borderless
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        //drag it here
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+
+        });
+        primaryStage.show();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
